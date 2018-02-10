e2af22fd0157446af825bfe068a6bd4ba282709d
Jade <jade@emmental.co.kr>
2018-02-10 09:47:56 +0900

    [@packages/utils] mysql 모듈명 수정

--- packages/utils/mysql.util.js
+++ packages/utils/mysql.util.js
@@ -2,15 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql = require("mysql");
 /** MySQL DB 연결 */
-var mysqlResource;
-(function (mysqlResource) {
-    mysqlResource.conn = mysql.createConnection({
+var mysqlUtil;
+(function (mysqlUtil) {
+    mysqlUtil.conn = mysql.createConnection({
         host: 'h6.cpgkhytb1iav.ap-northeast-2.rds.amazonaws.com',
         user: 'h6_root',
         password: 'h6mysqldb',
         database: 'h6',
         timezone: 'UTC'
     });
-    mysqlResource.conn.connect();
-})(mysqlResource = exports.mysqlResource || (exports.mysqlResource = {}));
+    mysqlUtil.conn.connect();
+})(mysqlUtil = exports.mysqlUtil || (exports.mysqlUtil = {}));
 //# sourceMappingURL=mysql.util.js.map
--- packages/utils/mysql.util.ts
+++ packages/utils/mysql.util.ts
@@ -1,7 +1,7 @@
 import * as mysql from 'mysql';
 
 /** MySQL DB 연결 */
-export module mysqlResource {
+export module mysqlUtil {
 	export const conn = mysql.createConnection({
         host     : 'h6.cpgkhytb1iav.ap-northeast-2.rds.amazonaws.com',
         user     : 'h6_root',
