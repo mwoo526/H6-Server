bbd4566da9a9e0f024ab3a8e34f4e7d1c5f407a4
Jade <jade@emmental.co.kr>
2018-02-21 22:47:22 +0900

    [@apis/lectureInfo] 모델 추가 및 테스트 코드 추가

---
+++ apis/lectureInfo/model/lectureInfo.model.js
@@ -0,0 +1,179 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mysql_util_1 = require("../../../packages/utils/mysql.util");
+const pool = mysql_util_1.mysqlUtil.pool;
+// TODO(@jade): textbook 칼럼 추가  date: 2018. 2. 21. 오후 6:11
+class LectureInfo {
+    /**
+     * model: lectureInfo 생성
+     * @param lectureInfoData
+     * @returns {Promise<void>}
+     */
+    createLectureInfo(lectureInfoData) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield pool.getConnection(function (err, connection) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield connection.query(`INSERT INTO lecturesInfo SET ?`, lectureInfoData, function (err) {
+                        if (err) {
+                            connection.release();
+                            reject(err);
+                        }
+                        else {
+                            connection.release();
+                            resolve(lectureInfoData);
+                        }
+                    });
+                });
+            });
+        }));
+    }
+    /**
+     * model: lectureInfo 리스트 조회
+     * @returns {Promise<void>}
+     */
+    listLectureInfo() {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield pool.getConnection(function (err, connection) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield connection.query(`SELECT t1.lectureInfoIndex, t1.average, t2.lectureName, t2.track, t3.professorName FROM lecturesInfo AS t1 INNER JOIN lectures AS t2 ON t1.lectureIndex = t2.lectureIndex INNER JOIN professors AS t3 ON t1.professorIndex = t3.professorIndex`, function (err, rows) {
+                        if (err) {
+                            connection.release();
+                            reject(err);
+                        }
+                        else {
+                            connection.release();
+                            resolve(rows);
+                        }
+                    });
+                });
+            });
+        }));
+    }
+    /**
+     * model: lectureInfo index 조회
+     * @param lectureInfoIndex
+     * @returns {Promise<void>}
+     */
+    getLectureInfoByLectureInfoIndex(lectureInfoIndex) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield pool.getConnection(function (err, connection) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield connection.query(`SELECT t1.lectureInfoIndex, t1.average, t2.lectureName, t2.track, t3.professorName FROM lecturesInfo AS t1 INNER JOIN lectures AS t2 ON t1.lectureIndex = t2.lectureIndex INNER JOIN professors AS t3 ON t1.professorIndex = t3.professorIndex WHERE t1.lectureInfoIndex = ${lectureInfoIndex}`, function (err, rows) {
+                        if (err) {
+                            connection.release();
+                            reject(err);
+                        }
+                        else {
+                            connection.release();
+                            resolve(rows);
+                        }
+                    });
+                });
+            });
+        }));
+    }
+    /**
+     * model: lectureInfo lectureName 조회
+     * @param lectureName
+     * @returns {Promise<void>}
+     */
+    getLectureInfoByLectureName(lectureName) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield pool.getConnection(function (err, connection) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield connection.query(`SELECT t1.lectureInfoIndex, t1.average, t2.lectureName, t2.track, t3.professorName FROM lecturesInfo AS t1 INNER JOIN lectures AS t2 ON t1.lectureIndex = t2.lectureIndex INNER JOIN professors AS t3 ON t1.professorIndex = t3.professorIndex WHERE t2.lectureName = '${lectureName}'`, function (err, rows) {
+                        if (err) {
+                            connection.release();
+                            reject(err);
+                        }
+                        else {
+                            connection.release();
+                            resolve(rows);
+                        }
+                    });
+                });
+            });
+        }));
+    }
+    /**
+     * model: lectureInfo professorName 조회
+     * @param professorName
+     * @returns {Promise<void>}
+     */
+    getLectureInfoByProfessorName(professorName) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield pool.getConnection(function (err, connection) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield connection.query(`SELECT t1.lectureInfoIndex, t1.average, t2.lectureName, t2.track, t3.professorName FROM lecturesInfo AS t1 INNER JOIN lectures AS t2 ON t1.lectureIndex = t2.lectureIndex INNER JOIN professors AS t3 ON t1.professorIndex = t3.professorIndex WHERE t3.professorName = '${professorName}'`, function (err, rows) {
+                        if (err) {
+                            connection.release();
+                            reject(err);
+                        }
+                        else {
+                            connection.release();
+                            resolve(rows);
+                        }
+                    });
+                });
+            });
+        }));
+    }
+    /**
+     * model: lectureInfo 업데이트
+     * @param {number} lectureInfoIndex
+     * @param lectureInfoData
+     * @returns {Promise<void>}
+     */
+    updateLectureInfo(lectureInfoIndex, lectureInfoData) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield pool.getConnection(function (err, connection) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield connection.query(`UPDATE lecturesInfo SET ? WHERE lectureInfoIndex = ?`, [lectureInfoData, lectureInfoIndex], function (err) {
+                        if (err) {
+                            connection.release();
+                            reject(err);
+                        }
+                        else {
+                            connection.release();
+                            resolve(lectureInfoData);
+                        }
+                    });
+                });
+            });
+        }));
+    }
+    /**
+     * model: lectureInfo 삭제
+     * @param {number} lectureInfoIndex
+     * @returns {Promise<void>}
+     */
+    deleteLectureInfo(lectureInfoIndex) {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            yield pool.getConnection(function (err, connection) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield connection.query('DELETE FROM lecturesInfo WHERE lectureInfoIndex = ?', lectureInfoIndex, function (err, rows) {
+                        if (err) {
+                            connection.release();
+                            reject(err);
+                        }
+                        else {
+                            connection.release();
+                            resolve(rows);
+                        }
+                    });
+                });
+            });
+        }));
+    }
+}
+exports.LectureInfo = LectureInfo;
+exports.lectureInfo = new LectureInfo();
+//# sourceMappingURL=lectureInfo.model.js.map
