a13cecf87c5d0ea4e85fdf3081c0409e40258a5a
Jade <jade@emmental.co.kr>
2018-02-24 02:26:08 +0900

    [@apis/lectureInfo] 누락된 라우트 추가

--- apis/lectureInfo/route/lectureInfo.route.ts
+++ apis/lectureInfo/route/lectureInfo.route.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { LectureInfoResource } from '../../../resources/lectureInfo.resource';
 import { lectureInfo } from '../model/lectureInfo.model';
 
 export class LectureInfoRoutes {
@@ -9,14 +10,72 @@ export class LectureInfoRoutes {
 	}
 
 	public router() {
+		this.lectureInfoRouter.post('/lecturesInfo', createLectureInfo);
+		this.lectureInfoRouter.get('/lecturesInfo', listLectureInfo);
+		this.lectureInfoRouter.get('/lecturesInfo/lectureInfoIndex/:lectureInfoIndex', getLectureInfoByLectureInfoIndex);
 		this.lectureInfoRouter.get('/lecturesInfo/lectureName/:lectureName', getLectureInfoByLectureName);
 		this.lectureInfoRouter.get('/lecturesInfo/professorName/:professorName', getLectureInfoByProfessorName);
+		this.lectureInfoRouter.put('/lecturesInfo/:lectureInfoIndex', updateLectureInfo);
+		this.lectureInfoRouter.delete('/lecturesInfo/:lectureInfoIndex', deleteLectureInfo);
 	}
 }
 
-async function getLectureInfoByLectureName(req, res): Promise<void> {
+/**
+ * route: lectureInfo 생성
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+async function createLectureInfo(req, res): Promise<void> {
+	let lectureInfoData: any = new LectureInfoResource(req.body);
 	try {
-		let lectureName: string = req.params.lectureName;
+		const result = await lectureInfo.createLectureInfo(lectureInfoData.getLectureInfo());
+		res.send(result);
+	} catch (err) {
+		res.send(err);
+	}
+}
+
+/**
+ * route: lectureInfo 리스트 조회
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+async function listLectureInfo(req, res): Promise<void> {
+	try {
+		const result = await lectureInfo.listLectureInfo();
+		res.send(result);
+	} catch (err) {
+		res.send(err);
+	}
+}
+
+/**
+ * route: lectureInfo index 조회
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+async function getLectureInfoByLectureInfoIndex(req, res): Promise<void> {
+	let lectureInfoIndex: number = req.params.lectureInfoIndex;
+	try {
+		const result = await lectureInfo.getLectureInfoByLectureInfoIndex(lectureInfoIndex);
+		res.send(result);
+	} catch (err) {
+		res.send(err);
+	}
+}
+
+/**
+ * route: lectureInfo lectureName 조회
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+async function getLectureInfoByLectureName(req, res): Promise<void> {
+	let lectureName: string = req.params.lectureName;
+	try {
 		const result = await lectureInfo.getLectureInfoByLectureName(lectureName);
 		res.send(result);
 	} catch (err) {
@@ -24,10 +83,49 @@ async function getLectureInfoByLectureName(req, res): Promise<void> {
 	}
 }
 
+/**
+ * route: lectureInfo professorName 조회
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
 async function getLectureInfoByProfessorName(req, res): Promise<void> {
+	let professorName: string = req.params.professorName;
 	try {
-		let professorName: string = req.params.professorName;
 		const result = await lectureInfo.getLectureInfoByProfessorName(professorName);
+		res.send(result);
+	} catch (err) {
+		res.send(err);
+	}
+}
+
+/**
+ * route: lectureInfo 업데이트
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+async function updateLectureInfo(req, res): Promise<void> {
+	let lectureInfoIndex: number = req.params.lectureInfoIndex;
+	let lectureInfoData: any = new LectureInfoResource(req.body);
+	try {
+		const result = await lectureInfo.updateLectureInfo(lectureInfoIndex, lectureInfoData);
+		res.send(result);
+	} catch (err) {
+		res.send(err);
+	}
+}
+
+/**
+ * route: lectureInfo 삭제
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+async function deleteLectureInfo(req, res): Promise<void> {
+	let lectureInfoIndex: number = req.params.lectureInfoIndex;
+	try {
+		const result = await lectureInfo.deleteLectureInfo(lectureInfoIndex);
 		res.send(result);
 	} catch (err) {
 		res.send(err);
