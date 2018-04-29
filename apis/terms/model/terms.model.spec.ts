896ce5f20b354fa7f2ed7da5537b756f3f24dd72
Jade <jade@emmental.co.kr>
2018-04-29 19:05:04 +0900

    [H6-server/apis] 약관 API 추가

---
+++ apis/terms/model/terms.model.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { loadTermsHtml } from './terms.model';
+
+describe('terms 모델', () => {
+	it('loadTermHtml', async () => {
+		const result = await loadTermsHtml('service');
+		// console.log(result);
+	});
+});
