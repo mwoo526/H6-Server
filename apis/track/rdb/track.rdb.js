e38d84b6b2295c18909a53552ebe5b31eb194613
JAESUNG AHN <kingdom0608@gmail.com>
2018-06-13 20:18:53 +0900

    [@apis/track] 트랙 시드 데이터 추가하는 코드 추가

--- apis/track/rdb/track.rdb.js
+++ apis/track/rdb/track.rdb.js
@@ -11,18 +11,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const chai_1 = require("chai");
 const track_model_1 = require("../model/track.model");
 describe('track 모델', () => {
-    let testTrackName = '경제학과';
+    let testTrackName = [];
+    testTrackName = [
+        '산업경영공학과',
+        'IT응용시스템공학과',
+        '컴퓨터공학과',
+        '기계시스템공학과'
+    ];
     it('createTrack', () => __awaiter(this, void 0, void 0, function* () {
-        const result = yield track_model_1.track.createTrack({
-            trackName: testTrackName
-        });
-        console.log(result);
-        chai_1.expect(result).instanceof(Object);
+        for (let i = 0; i < testTrackName.length; i++) {
+            const result = yield track_model_1.track.createTrack({
+                trackName: testTrackName[i]
+            });
+            // console.log(result);
+            chai_1.expect(result).instanceof(Object);
+        }
     }));
     it('deleteTrack', () => __awaiter(this, void 0, void 0, function* () {
-        const result = yield track_model_1.track.deleteTrack(testTrackName);
-        // console.log(result);
-        chai_1.expect(result).instanceof(Object);
+        for (let i = 0; i < testTrackName.length; i++) {
+            const result = yield track_model_1.track.deleteTrack(testTrackName[i]);
+            // console.log(result);
+            chai_1.expect(result).instanceof(Object);
+        }
     }));
 });
 //# sourceMappingURL=track.rdb.js.map
--- apis/track/rdb/track.rdb.ts
+++ apis/track/rdb/track.rdb.ts
@@ -2,19 +2,29 @@ import { expect } from 'chai';
 import { track } from '../model/track.model';
 
 describe('track 모델', () => {
-	let testTrackName: string = '경제학과';
+	let testTrackName: Array<string> = [];
+	testTrackName = [
+		'산업경영공학과',
+		'IT응용시스템공학과',
+		'컴퓨터공학과',
+		'기계시스템공학과'
+	];
 
 	it('createTrack', async () => {
-		const result = await track.createTrack({
-			trackName: testTrackName
-		});
-		console.log(result);
-		expect(result).instanceof(Object);
+		for (let i = 0; i < testTrackName.length; i++) {
+			const result = await track.createTrack({
+				trackName: testTrackName[i]
+			});
+			// console.log(result);
+			expect(result).instanceof(Object);
+		}
 	});
 
 	it('deleteTrack', async () => {
-		const result = await track.deleteTrack(testTrackName);
-		// console.log(result);
-		expect(result).instanceof(Object);
+		for (let i = 0; i < testTrackName.length; i++) {
+			const result = await track.deleteTrack(testTrackName[i]);
+			// console.log(result);
+			expect(result).instanceof(Object);
+		}
 	})
 });
