a23eea4159db581193979c7e4fc7c66bb242b472
JAESUNG AHN <kingdom0608@gmail.com>
2018-09-02 13:30:35 +0900

    [H6-server] gitignore 캐싱 업데이트

--- packages/utils/jwt.util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jwtToken;
-(function (jwtToken) {
-    jwtToken.secret = 'MySecret';
-    jwtToken.algorithm = 'HS256';
-    jwtToken.expiresln = '24h';
-})(jwtToken = exports.jwtToken || (exports.jwtToken = {}));
-//# sourceMappingURL=jwt.util.js.map
