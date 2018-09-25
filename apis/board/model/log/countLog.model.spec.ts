4105926a28846ddbc9ee45eee2e3bb3a1f94950e
JAESUNG AHN <kingdom0608@gmail.com>
2018-09-25 18:39:18 +0900

    [apis] 코드 인텐트 정리

--- apis/board/model/log/countLog.model.spec.ts
+++ apis/board/model/log/countLog.model.spec.ts
@@ -1,23 +1,23 @@
-import {expect} from 'chai';
-import {countLog} from "./countLog.model";
+import { expect } from 'chai';
+import { countLog } from './countLog.model';
 
 describe('countLog 모델', () => {
 
-    const countLogData: any = {
-        boardIndex: 19,
-        userIndex: 10
-    };
+	const countLogData: any = {
+		boardIndex: 19,
+		userIndex: 10
+	};
 
-    it('createCountLog', async () => {
-        const result: any = await countLog.createCountLog(countLogData.boardIndex,countLogData.userIndex);
-        // console.log(result);
-        expect(result).to.instanceof(Object);
-    })
+	it('createCountLog', async () => {
+		const result: any = await countLog.createCountLog(countLogData.boardIndex, countLogData.userIndex);
+		// console.log(result);
+		expect(result).to.instanceof(Object);
+	})
 
-    it('checkCountLog', async () => {
-        const result: any = await countLog.checkCountLog(countLogData.boardIndex,countLogData.userIndex);
-        // console.log(result);
-        expect(result).to.instanceof(Object);
-    })
+	it('checkCountLog', async () => {
+		const result: any = await countLog.checkCountLog(countLogData.boardIndex, countLogData.userIndex);
+		// console.log(result);
+		expect(result).to.instanceof(Object);
+	})
 
 })
