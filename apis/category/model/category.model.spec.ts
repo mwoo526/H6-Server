e8725b96bc2a86b1548b53ff26953aee4b495145
JAESUNG AHN <kingdom0608@gmail.com>
2018-11-13 23:54:18 +0900

    [apis] 카테고리 테스트코드 수정

--- apis/category/model/category.model.spec.ts
+++ apis/category/model/category.model.spec.ts
@@ -3,21 +3,21 @@ import { category } from './category.model';
 
 describe('category 모델', () => {
 	let categoryName: Array<string> = [
-		'자유',
-		'연애',
-		'취업'
+		'테스트자유',
+		'테스트연애',
+		'테스트취업'
 	];
 
 	let isCategoryName: Array<string> = [
-		'자유',
-		'연애',
-		'취업'
-	]
+		'테스트자유',
+		'테스트연애',
+		'테스트취업'
+	];
 
 	let updateCategoryName: Array<string> = [
-		'인문',
-		'사회',
-		'예술'
+		'테스트인문',
+		'테스트사회',
+		'테스트예술'
 	];
 
 	it('createCategory', async () => {
