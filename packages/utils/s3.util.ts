b6a23814ef47184ee8da9af062f4878bc2d293db
JAESUNG AHN <kingdom0608@gmail.com>
2018-12-02 18:02:24 +0900

    [H6-server] awsconfig 파일 경로 수정

--- packages/utils/s3.util.ts
+++ packages/utils/s3.util.ts
@@ -1,21 +1,25 @@
 import * as aws from 'aws-sdk';
+import * as fs from "fs";
 import * as multer from 'multer';
 import * as multerS3 from 'multer-s3';
 import * as path from 'path';
 
 export module s3Util {
 	aws.config.loadFromPath(__dirname + '/config/awsconfig.json');
+	const file = './packages/utils/config/awsconfig.json';
+	let awsconfig: any = fs.readFileSync(file, 'utf8');
+	awsconfig = JSON.parse(awsconfig);
 	let s3 = new aws.S3();
 
 	export const upload = multer({
 		storage: multerS3({
 			s3: s3,
-			bucket: 'dv-handam/avatar',
+			bucket: awsconfig.bucket,
 			key: function(req, file, cb) {
 				let extension = path.extname(file.originalname);
 				cb(null, Date.now().toString() + extension)
 			},
-			acl: 'public-read-write',
+			acl: awsconfig.acl,
 		})
 	});
 }
