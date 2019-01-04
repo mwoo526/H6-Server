e64ac471739dce5fecb76675c784a2fec2030a9f
JAESUNG AHN <kingdom0608@gmail.com>
2019-01-04 23:36:55 +0900

    [apis/today] 인텐트 정리

--- apis/todayLunch/route/todayLunch.route.ts
+++ apis/todayLunch/route/todayLunch.route.ts
@@ -1,21 +1,21 @@
 import * as express from 'express';
-import { TodayLunchResource } from "../../../resources/todayLunch.resource";
-import { todayLunch } from "../model/todayLunch.model";
+import { TodayLunchResource } from '../../../resources/todayLunch.resource';
+import { todayLunch } from '../model/todayLunch.model';
 
 export class TodayLunchRoutes {
-    public todayLunchRouter: express.Route = express.Router();
+	public todayLunchRouter: express.Route = express.Router();
 
-    constructor() {
-        this.router();
-    }
+	constructor() {
+		this.router();
+	}
 
-    public router() {
-        this.todayLunchRouter.post('/todayLunch', createTodayLunch);
-        this.todayLunchRouter.get('/todayLunch', getTodayLunches);
-        this.todayLunchRouter.get('/todayLunch/:todayLunchIndex', getTodayLunchByIndex);
-        this.todayLunchRouter.put('/todayLunch/:todayLunchIndex', updateTodayLunch);
-        this.todayLunchRouter.delete('/todayLunch/:todayLunchIndex', deleteTodayLunchByIndex);
-    }
+	public router() {
+		this.todayLunchRouter.post('/todayLunch', createTodayLunch);
+		this.todayLunchRouter.get('/todayLunch', getTodayLunches);
+		this.todayLunchRouter.get('/todayLunch/:todayLunchIndex', getTodayLunchByIndex);
+		this.todayLunchRouter.put('/todayLunch/:todayLunchIndex', updateTodayLunch);
+		this.todayLunchRouter.delete('/todayLunch/:todayLunchIndex', deleteTodayLunchByIndex);
+	}
 }
 
 /**
@@ -25,25 +25,25 @@ export class TodayLunchRoutes {
  * @returns {Promise<void>}
  */
 async function createTodayLunch(req, res): Promise<void> {
-    let todayLunchData: any = new TodayLunchResource(req.body);
-    try {
-        const result: any = await todayLunch.createTodayLunch(todayLunchData.getTodayLunch());
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'createTodayLunch: 200'
-        });
-    } catch (err) {
-        switch(err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'createTodayLunch: 500'
-                })
-        }
-    }
+	let todayLunchData: any = new TodayLunchResource(req.body);
+	try {
+		const result: any = await todayLunch.createTodayLunch(todayLunchData.getTodayLunch());
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'createTodayLunch: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'createTodayLunch: 500'
+				})
+		}
+	}
 }
 
 /**
@@ -53,24 +53,24 @@ async function createTodayLunch(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function getTodayLunches(req, res): Promise<void> {
-    try {
-        const results: any = await todayLunch.getTodayLunches();
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: results,
-            message: 'getTodayLunches: 200'
-        });
-    } catch (err) {
-        switch(err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'getTodayLunches: 500'
-                });
-        }
-    }
+	try {
+		const results: any = await todayLunch.getTodayLunches();
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: results,
+			message: 'getTodayLunches: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'getTodayLunches: 500'
+				});
+		}
+	}
 }
 
 /**
@@ -80,25 +80,25 @@ async function getTodayLunches(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function getTodayLunchByIndex(req, res): Promise<void> {
-    const todayLunchIndex: number = req.params.todayLunchIndex;
-    try {
-        const result: any = await todayLunch.getTodayLunchByIndex(todayLunchIndex);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'getTodayLunchByIndex: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'getTodayLunchByIndex: 500'
-                });
-        }
-    }
+	const todayLunchIndex: number = req.params.todayLunchIndex;
+	try {
+		const result: any = await todayLunch.getTodayLunchByIndex(todayLunchIndex);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'getTodayLunchByIndex: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'getTodayLunchByIndex: 500'
+				});
+		}
+	}
 }
 
 /**
@@ -108,26 +108,26 @@ async function getTodayLunchByIndex(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function updateTodayLunch(req, res): Promise<void> {
-    const todayLunchIndex: number = req.params.todayLunchIndex;
-    const todayLunchData: any = new TodayLunchResource(req.body);
-    try {
-        const result: any = await todayLunch.updateTodayLunch(todayLunchIndex, todayLunchData);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'updateTodayLunch: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'updateTodayLunch: 500'
-                });
-        }
-    }
+	const todayLunchIndex: number = req.params.todayLunchIndex;
+	const todayLunchData: any = new TodayLunchResource(req.body);
+	try {
+		const result: any = await todayLunch.updateTodayLunch(todayLunchIndex, todayLunchData);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'updateTodayLunch: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'updateTodayLunch: 500'
+				});
+		}
+	}
 }
 
 /**
@@ -137,26 +137,25 @@ async function updateTodayLunch(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function deleteTodayLunchByIndex(req, res): Promise<void> {
-    const todayLunchIndex: number = req.params.todayLunchIndex;
-    try {
-        const result: any = await todayLunch.deleteTodayLunch(todayLunchIndex);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'deleteTodayLunch: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'deleteTodayLunch: 500'
-                });
-        }
-    }
+	const todayLunchIndex: number = req.params.todayLunchIndex;
+	try {
+		const result: any = await todayLunch.deleteTodayLunch(todayLunchIndex);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'deleteTodayLunch: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'deleteTodayLunch: 500'
+				});
+		}
+	}
 }
-
 
 export const todayLunchRoutes: TodayLunchRoutes = new TodayLunchRoutes();
--- apis/todayLunch/route/todayLunchMenu.route.ts
+++ apis/todayLunch/route/todayLunchMenu.route.ts
@@ -1,23 +1,23 @@
 import * as express from 'express';
-import { TodayLunchMenuResource } from "../../../resources/todayLunchMenu.resource";
-import { todayLunchMenu } from "../model/todayLunchMenu.model";
+import { TodayLunchMenuResource } from '../../../resources/todayLunchMenu.resource';
+import { todayLunchMenu } from '../model/todayLunchMenu.model';
 
 export class TodayLunchMenuRoute {
-    public todayLunchMenuRouter: express.Route = express.Router();
+	public todayLunchMenuRouter: express.Route = express.Router();
 
-    constructor() {
-        this.router();
-    }
+	constructor() {
+		this.router();
+	}
 
-    public router() {
-        this.todayLunchMenuRouter.post('/todayLunchMenu', createTodayLunchMenu);
-        this.todayLunchMenuRouter.get('/todayLunchMenu', getTodayLunchMenus);
-        this.todayLunchMenuRouter.get('/todayLunchMenu/:todayLunchMenuIndex', getTodayLunchMenuByIndex);
-        this.todayLunchMenuRouter.get('/todayLunchMenu/todayLunch/:todayLunchIndex', getTodayLunchMenuByTodayLunchIndex);
-        this.todayLunchMenuRouter.put('/todayLunchMenu/:todayLunchMenuIndex', updateTodayLunchMenu);
-        this.todayLunchMenuRouter.delete('/todayLunchMenu/:todayLunchMenuIndex', deleteTodayLunchMenu);
-        this.todayLunchMenuRouter.delete('/todayLunchMenu/todayLunch/:todayLunchIndex', deleteTodayLunchMenuByTodayLunchIndex);
-    }
+	public router() {
+		this.todayLunchMenuRouter.post('/todayLunchMenu', createTodayLunchMenu);
+		this.todayLunchMenuRouter.get('/todayLunchMenu', getTodayLunchMenus);
+		this.todayLunchMenuRouter.get('/todayLunchMenu/:todayLunchMenuIndex', getTodayLunchMenuByIndex);
+		this.todayLunchMenuRouter.get('/todayLunchMenu/todayLunch/:todayLunchIndex', getTodayLunchMenuByTodayLunchIndex);
+		this.todayLunchMenuRouter.put('/todayLunchMenu/:todayLunchMenuIndex', updateTodayLunchMenu);
+		this.todayLunchMenuRouter.delete('/todayLunchMenu/:todayLunchMenuIndex', deleteTodayLunchMenu);
+		this.todayLunchMenuRouter.delete('/todayLunchMenu/todayLunch/:todayLunchIndex', deleteTodayLunchMenuByTodayLunchIndex);
+	}
 }
 
 /**
@@ -27,25 +27,25 @@ export class TodayLunchMenuRoute {
  * @returns {Promise<void>}
  */
 async function createTodayLunchMenu(req, res): Promise<void> {
-    let todayLunchMenuData: any = new TodayLunchMenuResource(req.body);
-    try {
-        const result: any = await todayLunchMenu.createTodayLunchMenu(todayLunchMenuData.getTodayLunchMenu());
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'createTodayLunchMenu: 200'
-        });
-    } catch (err) {
-        switch(err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'createTodayLunchMenu: 500'
-                })
-        }
-    }
+	let todayLunchMenuData: any = new TodayLunchMenuResource(req.body);
+	try {
+		const result: any = await todayLunchMenu.createTodayLunchMenu(todayLunchMenuData.getTodayLunchMenu());
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'createTodayLunchMenu: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'createTodayLunchMenu: 50000'
+				})
+		}
+	}
 }
 
 /**
@@ -55,24 +55,24 @@ async function createTodayLunchMenu(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function getTodayLunchMenus(req, res): Promise<void> {
-    try {
-        const results: any = await todayLunchMenu.getTodayLunchMenus();
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: results,
-            message: 'getTodayLunchMenus: 200'
-        });
-    } catch (err) {
-        switch(err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'getTodayLunchMenus: 500'
-                });
-        }
-    }
+	try {
+		const results: any = await todayLunchMenu.getTodayLunchMenus();
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: results,
+			message: 'getTodayLunchMenus: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'getTodayLunchMenus: 50000'
+				});
+		}
+	}
 }
 
 /**
@@ -82,25 +82,25 @@ async function getTodayLunchMenus(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function getTodayLunchMenuByIndex(req, res): Promise<void> {
-    const todayLunchMenuIndex: number = req.params.todayLunchMenuIndex;
-    try {
-        const result: any = await todayLunchMenu.getTodayLunchMenuByIndex(todayLunchMenuIndex);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'getTodayLunchMenuByIndex: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'getTodayLunchMenuByIndex: 500'
-                });
-        }
-    }
+	const todayLunchMenuIndex: number = req.params.todayLunchMenuIndex;
+	try {
+		const result: any = await todayLunchMenu.getTodayLunchMenuByIndex(todayLunchMenuIndex);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'getTodayLunchMenuByIndex: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'getTodayLunchMenuByIndex: 50000'
+				});
+		}
+	}
 }
 
 /**
@@ -110,27 +110,26 @@ async function getTodayLunchMenuByIndex(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function getTodayLunchMenuByTodayLunchIndex(req, res): Promise<void> {
-    const todayLunchIndex: number = req.params.todayLunchIndex;
-    try {
-        const results: any = await todayLunchMenu.getTodayLunchMenuByTodayLunchIndex(todayLunchIndex);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: results,
-            message: 'getTodayLunchByTodayLunchIndex: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'getTodayLunchByTodayLunchIndex: 500'
-                });
-        }
-    }
+	const todayLunchIndex: number = req.params.todayLunchIndex;
+	try {
+		const results: any = await todayLunchMenu.getTodayLunchMenuByTodayLunchIndex(todayLunchIndex);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: results,
+			message: 'getTodayLunchByTodayLunchIndex: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'getTodayLunchByTodayLunchIndex: 50000'
+				});
+		}
+	}
 }
-
 
 /**
  * route: todayLunchMenu 업데이트
@@ -139,28 +138,27 @@ async function getTodayLunchMenuByTodayLunchIndex(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function updateTodayLunchMenu(req, res): Promise<void> {
-    const todayLunchMenuIndex: number = req.params.todayLunchMenuIndex;
-    const todayLunchMenuData: any = new TodayLunchMenuResource(req.body);
-    try {
-        const result: any = await todayLunchMenu.updateTodayLunchMenu(todayLunchMenuIndex, todayLunchMenuData);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'updateTodayLunchMenu: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'updateTodayLunchMenu: 500'
-                });
-        }
-    }
+	const todayLunchMenuIndex: number = req.params.todayLunchMenuIndex;
+	const todayLunchMenuData: any = new TodayLunchMenuResource(req.body);
+	try {
+		const result: any = await todayLunchMenu.updateTodayLunchMenu(todayLunchMenuIndex, todayLunchMenuData);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'updateTodayLunchMenu: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'updateTodayLunchMenu: 50000'
+				});
+		}
+	}
 }
-
 
 /**
  * route: todayLunchMenu 삭제
@@ -169,27 +167,26 @@ async function updateTodayLunchMenu(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function deleteTodayLunchMenu(req, res): Promise<void> {
-    const todayLunchMenuIndex: number = req.params.todayLunchMenuIndex;
-    try {
-        const result: any = await todayLunchMenu.deleteTodayLunchMenu(todayLunchMenuIndex);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'deleteTodayLunchMenu: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'deleteTodayLunchMenu: 500'
-                });
-        }
-    }
+	const todayLunchMenuIndex: number = req.params.todayLunchMenuIndex;
+	try {
+		const result: any = await todayLunchMenu.deleteTodayLunchMenu(todayLunchMenuIndex);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'deleteTodayLunchMenu: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'deleteTodayLunchMenu: 50000'
+				});
+		}
+	}
 }
-
 
 /**
  * route: todayLunchIndex 에 해당하는 모든 todayLunchMenu 삭제
@@ -198,26 +195,25 @@ async function deleteTodayLunchMenu(req, res): Promise<void> {
  * @returns {Promise<void>}
  */
 async function deleteTodayLunchMenuByTodayLunchIndex(req, res): Promise<void> {
-    const todayLunchIndex: number = req.params.todayLunchIndex;
-    try {
-        const result: any = await todayLunchMenu.deleteTodayLunchMenuByTodayLunchIndex(todayLunchIndex);
-        res.send({
-            success: true,
-            statusCode: 200,
-            result: result,
-            message: 'deleteTodayLunchMenuByTodayLunchIndex: 200'
-        });
-    } catch (err) {
-        switch (err) {
-            default:
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'deleteTodayLunchMenuByTodayLunchIndex: 500'
-                });
-        }
-    }
+	const todayLunchIndex: number = req.params.todayLunchIndex;
+	try {
+		const result: any = await todayLunchMenu.deleteTodayLunchMenuByTodayLunchIndex(todayLunchIndex);
+		res.send({
+			success: true,
+			statusCode: 200,
+			result: result,
+			message: 'deleteTodayLunchMenuByTodayLunchIndex: 200'
+		});
+	} catch (err) {
+		switch (err) {
+			default:
+				res.send({
+					success: false,
+					statusCode: 500,
+					message: 'deleteTodayLunchMenuByTodayLunchIndex: 50000'
+				});
+		}
+	}
 }
-
 
 export const todayLunchMenuRoutes: TodayLunchMenuRoute = new TodayLunchMenuRoute();
