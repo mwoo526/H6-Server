89e190a8f9ff4a019e729a983a2dc85d3f334f6b
mwoo526 <mwoo526@naver.com>
2019-01-05 15:07:21 +0900

    [serverless] aws lambda, api gatway 프록시 api 구현

---
+++ lambda.ts
@@ -0,0 +1,8 @@
+import * as serverless from 'serverless-http';
+import * as express from 'express';
+import { Server } from './app';
+
+
+const app: express.Application = new Server().app;
+
+module.exports.handler = serverless(app);
